008309bcbf47332943f90be779ecd14eed958463
devsomsak <devrleang@gmail.com>
2024-01-03 13:39:06 +0700

    feat:solve react-hh-context-api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,46 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
-
+import { userDataContext } from "./context-api/userDataContext.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import ViewProductPage from "./pages/ViewProductPage.jsx";
 
 function App() {
-  const userData = {
-    username: "John",
-    avatar: "https://placedog.net/100/100",
-    level: "platinum",
-  };
+  const [userDatas, setUserDatas] = useState({});
+
+  useEffect(() => {
+    const userData = {
+      username: "John",
+      avatar: "https://placedog.net/100/100",
+      level: "platinum",
+    };
+    setUserDatas(userData);
+    console.log(userData)
+  }, []); // Empty dependency array ensures this effect runs only once on mount
 
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/product/view/:id" element={<ViewProductPage />} />
-        </Routes>
-      </Router>
-    </div>
+    <>
+      <userDataContext.Provider
+        value={{
+          userDataState: userDatas
+        }}
+      >
+        <div className="App">
+          <Router>
+            <Routes>
+              <Route
+                path="/"
+                element={<HomePage userDataState={userDatas} />}
+              />
+              <Route
+                path="/product/view/:id"
+                element={<ViewProductPage userDataState={userDatas} />}
+              />
+            </Routes>
+          </Router>
+        </div>
+      </userDataContext.Provider>
+    </>
   );
 }
 
--- client/src/components/AppHeader.jsx
+++ client/src/components/AppHeader.jsx
@@ -1,8 +1,15 @@
+import { useContext } from "react";
+import { userDataContext } from "../context-api/userDataContext";
+
 function AppHeader() {
+
+  const contextData = useContext(userDataContext);
+  const username = contextData.userDataState?.username ?? "Guest";
+  
   return (
     <div className="app-header-container">
       <h1 className="app-title">Products</h1>
-      <h2>สวัสดีคุณ (x)</h2>
+      <h2>สวัสดีคุณ {username}</h2>
     </div>
   );
 }
